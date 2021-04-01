c1809483710fd65e6b9aa2608db5ebe605c846f0
yurii-shendryk <yuriishendryk93@gmail.com>
2021-04-01 16:36:07 +0300

    update handleFeedback function and delete data-attributes from buttons

--- src/App.js
+++ src/App.js
@@ -7,13 +7,8 @@ class App extends Component {
   state = { good: 0, neutral: 0, bad: 0 };
 
   handleFeedback = ({ target }) => {
-    console.dir(target);
-    const { feedback } = target.dataset;
-    this.setState(prevState => {
-      return {
-        [feedback]: prevState[feedback] + 1,
-      };
-    });
+    const feedback = target.textContent;
+    this.setState(prevState => ({ [feedback]: prevState[feedback] + 1 }));
   };
 
   countTotalFeedback = () => {
--- src/components/FeedbackButton/FeedbackButton.jsx
+++ src/components/FeedbackButton/FeedbackButton.jsx
@@ -6,7 +6,6 @@ const FeedbackButton = ({ feedbackName, onLeaveFeedback }) => (
     <button
       className={styles.feedback__button}
       type="button"
-      data-feedback={feedbackName}
       onClick={onLeaveFeedback}
     >
       {feedbackName}
